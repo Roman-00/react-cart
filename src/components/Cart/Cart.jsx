e750976f3a2c185c3acc478e5374f0b3c37a0e77
Roman-00 <rvasin00@gmail.com>
2021-09-01 08:17:23 +0600

    add a cart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/* Импортируем стили */
+import styles from './Cart.module.css';
+
+export const Cart = (props) => {
+
+    const cartItems = []
+
+    return (
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-export const MealItemForm = () => {
+/* Импортируем компоненты */
+import { Input } from '../../UI/Input';
+
+/* Импортируем стили */
+import styles from './MealItemForm.module.css';
+
+export const MealItemForm = (props) => {
     return (
-        <div>
-            
-        </div>
+        <form className={styles.form}>
+            <Input label="Amount" input={{ 
+                id: 'amount',
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+             }}/>
+            <button>+ Add</button>
+        </form>
     );
 };
