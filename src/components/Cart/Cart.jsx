ec66941151d6646ba41492d4d498c143a65e0632
Roman <rvasin00@gmail.com>
2021-09-01 15:49:07 +0600

    feact: add a cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,11 +5,15 @@ import styles from './Cart.module.css';
 
 export const Cart = (props) => {
 
-    const cartItems = []
+    const cartItems = [
+        { id: 'c1', name: 'Sushi', amount: 2, price: 12.99 },
+    ].map((item) => <li>{item.name}</li>)
 
     return (
         <div>
-            <div></div>
+            <div>
+                {cartItems}
+            </div>
             <div></div>
         </div>
     );
